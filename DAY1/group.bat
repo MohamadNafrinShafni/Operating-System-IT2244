45861025e9f9c6ad761e3651a4b1a162e07731a9
MohamadNafrinShafni <nafrinn03@gmail.com>
2025-04-30 20:41:52 +0530

    created DAY1 Folder with js files

---
+++ DAY1/group.bat
@@ -0,0 +1,39 @@
+mkdir critera_1
+cd critera_1
+mkdir Standard_1
+mkdir Standard_2
+mkdir Standard_3
+mkdir Standard_4
+mkdir Standard_5
+mkdir Standard_6
+cd..
+mkdir critera_2
+cd critera_2
+mkdir Standard_1
+mkdir Standard_2
+mkdir Standard_3
+mkdir Standard_4
+cd..
+mkdir critera_3
+cd critera_3
+mkdir Standard_1
+mkdir Standard_2
+mkdir Standard_3
+mkdir Standard_4
+cd..
+mkdir critera_4
+cd critera_4
+mkdir Standard_1
+mkdir Standard_2
+mkdir Standard_3
+mkdir Standard_4
+cd..
+mkdir critera_5
+cd critera_5
+mkdir Standard_1
+mkdir Standard_2
+mkdir Standard_3
+mkdir Standard_4
+mkdir critera_6
+cd..
+cd critera_6
